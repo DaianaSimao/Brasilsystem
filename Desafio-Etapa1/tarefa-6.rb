1660f2848736832e00981162b565ae443d65a299
DaianaSimão <107774354+DaianaSimao@users.noreply.github.com>
2023-03-25 13:28:00 -0300

    Tarefa 6

---
+++ Desafio-Etapa1/tarefa-6.rb
@@ -0,0 +1,16 @@
+#Tarefa - 6: Faça um programa que, dado um número a partir do qual deve-se começar a
+#    contagem regressiva, imprima na tela a contagem regressiva desse número até 0. Ao 
+#    final da contagem, imprima a mensagem KABUM.
+#    Importante: cada número deve ser impresso em uma nova linha, assim como a mensagem
+
+numero = gets.to_i
+num = []
+
+for i in 0..numero
+    num << numero
+    numero-=1
+end
+
+puts num << "KABUM"
+
+    
