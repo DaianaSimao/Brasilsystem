04cb0fe2ced46cb563442311a78d397d65738f33
DaianaSimão <107774354+DaianaSimao@users.noreply.github.com>
2023-03-25 13:18:39 -0300

    Tarefa 2

---
+++ Desafio-Etapa1/tarefa-2.rb
@@ -0,0 +1,7 @@
+#Tarefa - 2: Escreva um algoritmo que aceita uma string e retorne os caracteres na ordem
+#inversa
+
+
+palavra = gets.chomp.to_s #recebe o nome e retorna uma nova string sem separador de registros
+
+puts "#{palavra.reverse}" # O metodo reverse retorna a string ao contrario
